ece41836fba17e32584c8479153b733dddda4a12
Shubham Kumar <imshubhamkr50@gmail.com>
2024-08-24 13:43:13 +0530

    Implement bot playing strategy

--- src/strategies/bot/BotPlayingStrategy.java
+++ src/strategies/bot/BotPlayingStrategy.java
@@ -1,4 +1,8 @@
 package strategies.bot;
 
+import models.Board;
+import models.Move;
+
 public interface BotPlayingStrategy {
+    Move suggestMove(Board board);
 }
---
+++ src/strategies/bot/BotPlayingStrategyFactory.java
@@ -0,0 +1,9 @@
+package strategies.bot;
+
+import models.BotDifficultyLevel;
+
+public class BotPlayingStrategyFactory {
+    public static BotPlayingStrategy getBotPlayingStrategy(BotDifficultyLevel botDifficultyLevel) {
+        return new EasyBotPlayingStrategy();
+    }
+}
